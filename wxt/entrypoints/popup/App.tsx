48e5d680e343a13f2f8e17f556f6da7d3f87deb7
Atreyaa-Avs <atreyaaavs@gmail.com>
2026-01-22 09:34:42 +0530

    Logo svg

--- wxt/entrypoints/popup/App.tsx
+++ wxt/entrypoints/popup/App.tsx
@@ -1,35 +1,13 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
-import './App.css';
+import React from 'react'
+import Logo from "@/assets/logo.svg"
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <div className='flex'>
-        <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo size-44" alt="WXT logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>WXT + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 10)}>
-          count is {count + 10}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the WXT and React logos to learn more
-      </p>
-    </>
-  );
+    <div className='flex items-center justify-center gap-4 p-4'>
+      <img src={Logo} className='h-20 w-28' alt="Logo" />
+      <h1>Phishing URL Detector</h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
